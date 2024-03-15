24be9ef0af4318820da678a5a2f11517351d23fb
seeta-ramaraju <150829444+seeta-ramaraju@users.noreply.github.com>
2024-03-15 16:18:10 +0000

    Add Panel page

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import HomePageView from "../views/HomePageView.vue";
 import PublicationsView from "../views/PublicationsView.vue";
 import AboutProjectView from "../views/AboutProjectView.vue";
 import GeneView from "../views/GeneView.vue";
+import PanelView from "../views/PanelView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -26,6 +27,11 @@ const router = createRouter({
       path: "/gene/:symbol",
       name: "gene symbol",
       component: GeneView,
+    },
+    {
+      path: "/panel/:panel",
+      name: "panel",
+      component: PanelView,
     },
   ],
 });
