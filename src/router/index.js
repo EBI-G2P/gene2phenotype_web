ccf57ba779cfacbaa2881fcc2a8f8f9b091b367b
seeta-ramaraju <150829444+seeta-ramaraju@users.noreply.github.com>
2024-03-15 16:33:31 +0000

    Add LocusGeneDisease page

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import PublicationsView from "../views/PublicationsView.vue";
 import AboutProjectView from "../views/AboutProjectView.vue";
 import GeneView from "../views/GeneView.vue";
 import PanelView from "../views/PanelView.vue";
+import LocusGeneDiseaseView from "../views/LocusGeneDiseaseView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -32,6 +33,11 @@ const router = createRouter({
       path: "/panel/:panel",
       name: "panel",
       component: PanelView,
+    },
+    {
+      path: "/lgd/:stableId",
+      name: "locus gene disease",
+      component: LocusGeneDiseaseView,
     },
   ],
 });
