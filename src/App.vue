c59bb7267e2ea57a1d42e1b1e092e491cab68541
seeta-ramaraju <150829444+seeta-ramaraju@users.noreply.github.com>
2025-07-24 14:51:54 +0100

    Remove EMBL-EBI 2025 survey banner

--- src/App.vue
+++ src/App.vue
@@ -6,23 +6,6 @@ import HeaderWithoutSearch from "./components/header/HeaderWithoutSearch.vue";
 </script>
 
 <template>
-  <!-- Survey banner start -->
-  <div
-    class="alert alert-light mb-0 text-center p-2"
-    style="font-size: 12px"
-    role="alert"
-  >
-    We need your help! Has Gene2Phenotype saved you time or effort? Please take
-    15 minutes to fill in a survey and help EMBL-EBI make the case for why open
-    data resources are critical to life science research. <br />
-    <a
-      href="https://www.surveymonkey.com/r/QGFMBH8?channel=[webpage]"
-      class="alert-link"
-      target="_blank"
-      >https://www.surveymonkey.com/r/QGFMBH8?channel=[webpage]
-    </a>
-  </div>
-  <!-- Survey banner end -->
   <HeaderWithoutSearch v-if="$route.path === '/'" />
   <Header v-else />
   <RouterView />
