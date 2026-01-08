4f6e425217dd6d5b18ab278d3f30701b4c11fc22
seeta-ramaraju <150829444+seeta-ramaraju@users.noreply.github.com>
2026-01-08 11:52:30 +0000

    Update footer copyright text

--- src/components/footer/Footer.vue
+++ src/components/footer/Footer.vue
@@ -174,7 +174,7 @@
       <div
         class="d-flex flex-column flex-sm-row pt-3 pb-2 mt-3 mb-2 border-top"
       >
-        <p>&copy; EMBL-EBI 2025</p>
+        <p>&copy; EMBL-EBI 2026</p>
       </div>
     </div>
   </footer>
