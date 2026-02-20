61a070c8959b47c9eac0b4273457806d8a1686ea
seeta-ramaraju <150829444+seeta-ramaraju@users.noreply.github.com>
2026-02-20 11:27:32 +0000

    In curation page, update display when there are no saved drafts/published records for Gene

--- src/components/curation/ExistingGeneInformation.vue
+++ src/components/curation/ExistingGeneInformation.vue
@@ -141,259 +141,9 @@ export default {
     </div>
     <div v-else>
       <div v-if="!geneExistingDrafts && !geneExistingRecords">
-        <p class="pt-3">
+        <div class="alert alert-primary mt-3" role="alert">
           <i class="bi bi-info-circle"></i> There are no saved drafts or
           published records for this Gene.
-        </p>
-      </div>
-      <div v-if="geneExistingDrafts?.results?.length > 0">
-        <h3 class="pt-3">Saved drafts for Gene</h3>
-        <div class="mx-1 pt-3 table-responsive-xl">
-          <table class="table table-hover table-bordered">
-            <thead>
-              <tr>
-                <th>G2P ID <ToolTip :toolTipText="HELP_TEXT.G2P_ID" /></th>
-                <th>Gene</th>
-                <th>Disease</th>
-                <th>
-                  Allelic Requirement
-                  <ToolTip :toolTipText="HELP_TEXT.ALLELIC_REQUIREMENT" />
-                </th>
-                <th>Panels</th>
-                <th>
-                  Confidence <ToolTip :toolTipText="HELP_TEXT.CONFIDENCE" />
-                </th>
-                <th>Curator</th>
-                <th>Action</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr
-                v-for="item in geneExistingDrafts.results"
-                :key="item.stable_id"
-              >
-                <td>
-                  {{ item.stable_id }}
-                </td>
-                <td>
-                  {{ item.gene }}
-                </td>
-                <td>
-                  {{ item.disease_name }}
-                </td>
-                <td>{{ item.genotype }}</td>
-                <td>
-                  <span v-if="item.panels?.length > 0">
-                    {{ item.panels.join(", ") }}
-                  </span>
-                </td>
-                <td>
-                  <span
-                    v-if="item.confidence"
-                    class="badge text-white"
-                    :style="{
-                      backgroundColor:
-                        CONFIDENCE_COLOR_MAP[item.confidence.toLowerCase()],
-                    }"
-                  >
-                    {{ item.confidence }}
-                  </span>
-                </td>
-                <td>
-                  Name: {{ item.curator_first + " " + item.curator_last_name }}
-                  <br />
-                  Email: {{ item.curator_email }}
-                </td>
-                <td class="text-nowrap">
-                  <router-link
-                    :to="`/lgd/update-draft/${item.stable_id}`"
-                    style="text-decoration: none"
-                    v-if="isUserCreatorOfDraft(item.curator_email)"
-                  >
-                    Update draft
-                  </router-link>
-                  <a
-                    :href="`mailto:${item.curator_email}`"
-                    style="text-decoration: none"
-                    target="_blank"
-                    v-else
-                  >
-                    Mail curator
-                  </a>
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-        <div class="row mx-1 justify-content-between">
-          <div class="col-2 px-0">
-            <button
-              type="button"
-              class="btn btn-primary"
-              style="float: left"
-              @click="fetchExistingGeneDraftsPreviousPage"
-              v-if="geneExistingDrafts.previous"
-            >
-              <i class="bi bi-arrow-left"></i> Previous Page
-            </button>
-          </div>
-          <div class="col-2 px-0">
-            <button
-              type="button"
-              class="btn btn-primary"
-              style="float: right"
-              @click="fetchExistingGeneDraftsNextPage"
-              v-if="geneExistingDrafts.next"
-            >
-              Next Page
-              <i class="bi bi-arrow-right"></i>
-            </button>
-          </div>
-        </div>
-      </div>
-      <div v-if="geneExistingRecords?.results?.length > 0">
-        <h3 class="pt-3">Published records for Gene</h3>
-        <p class="text-muted mb-0">
-          View existing records to add to your panel
-        </p>
-        <div class="mx-1 pt-3 table-responsive-xl">
-          <table class="table table-hover table-bordered">
-            <thead>
-              <tr>
-                <th>G2P ID <ToolTip :toolTipText="HELP_TEXT.G2P_ID" /></th>
-                <th>Gene</th>
-                <th>Disease</th>
-                <th>
-                  Allelic Requirement
-                  <ToolTip :toolTipText="HELP_TEXT.ALLELIC_REQUIREMENT" />
-                </th>
-                <th>
-                  Mechanism <ToolTip :toolTipText="HELP_TEXT.MECHANISM" />
-                </th>
-                <th>Panels</th>
-                <th>
-                  Confidence <ToolTip :toolTipText="HELP_TEXT.CONFIDENCE" />
-                </th>
-                <th>Action</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr
-                v-for="item in geneExistingRecords.results"
-                :key="item.stable_id"
-              >
-                <td>
-                  {{ item.stable_id }}
-                </td>
-                <td>
-                  <router-link
-                    v-if="item.gene"
-                    :to="`/gene/${item.gene}`"
-                    style="text-decoration: none"
-                  >
-                    {{ item.gene }}
-                  </router-link>
-                </td>
-                <td>
-                  <router-link
-                    v-if="item.disease"
-                    :to="`/disease/${item.disease}`"
-                    style="text-decoration: none"
-                  >
-                    {{ item.disease }}
-                  </router-link>
-                </td>
-                <td>{{ item.genotype }}</td>
-                <td>{{ item.mechanism }}</td>
-                <td>
-                  <span v-if="item.panel?.length > 0">
-                    <span v-for="(panelName, index) in item.panel">
-                      <span v-if="index < item.panel.length - 1">
-                        <router-link
-                          :to="`/panel/${panelName}`"
-                          style="text-decoration: none"
-                        >
-                          {{ panelName }} </router-link
-                        >,
-                      </span>
-                      <router-link
-                        :to="`/panel/${panelName}`"
-                        style="text-decoration: none"
-                        v-else
-                      >
-                        {{ panelName }}
-                      </router-link>
-                    </span>
-                  </span>
-                </td>
-                <td>
-                  <span
-                    v-if="item.confidence"
-                    class="badge text-white"
-                    :style="{
-                      backgroundColor:
-                        CONFIDENCE_COLOR_MAP[item.confidence.toLowerCase()],
-                    }"
-                  >
-                    {{ item.confidence }}
-                  </span>
-                </td>
-                <td class="text-nowrap">
-                  <router-link
-                    :to="`/lgd/${item.stable_id}`"
-                    style="text-decoration: none"
-                    target="_blank"
-                    v-if="item.stable_id"
-                  >
-                    View record <i class="bi bi-box-arrow-up-right"></i>
-                  </router-link>
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-        <div class="row mx-1 justify-content-between">
-          <div class="col-2 px-0">
-            <button
-              type="button"
-              class="btn btn-primary"
-              style="float: left"
-              @click="fetchExistingGeneRecordsPreviousPage"
-              v-if="geneExistingRecords.previous"
-            >
-              <i class="bi bi-arrow-left"></i> Previous Page
-            </button>
-          </div>
-          <div class="col-2 px-0">
-            <button
-              type="button"
-              class="btn btn-primary"
-              style="float: right"
-              @click="fetchExistingGeneRecordsNextPage"
-              v-if="geneExistingRecords.next"
-            >
-              Next Page
-              <i class="bi bi-arrow-right"></i>
-            </button>
-          </div>
-        </div>
-      </div>
-      <div class="my-3">
-        <h6 class="mb-3">
-          Adding a novel disease or an association with a different mechanism or
-          allelic requirement?
-        </h6>
-        <div>
-          <button
-            type="button"
-            class="btn btn-link m-0 p-0"
-            style="text-decoration: none"
-            data-bs-toggle="modal"
-            data-bs-target="#record-guidelines-modal"
-          >
-            Please read this information before creating a new record
-            <i class="bi bi-file-earmark-text"></i>
-          </button>
         </div>
         <button
           type="button"
@@ -402,6 +152,266 @@ export default {
         >
           Create new record
         </button>
+      </div>
+      <div v-else>
+        <div v-if="geneExistingDrafts?.results?.length > 0">
+          <h3 class="pt-3">Saved drafts for Gene</h3>
+          <div class="mx-1 pt-3 table-responsive-xl">
+            <table class="table table-hover table-bordered">
+              <thead>
+                <tr>
+                  <th>G2P ID <ToolTip :toolTipText="HELP_TEXT.G2P_ID" /></th>
+                  <th>Gene</th>
+                  <th>Disease</th>
+                  <th>
+                    Allelic Requirement
+                    <ToolTip :toolTipText="HELP_TEXT.ALLELIC_REQUIREMENT" />
+                  </th>
+                  <th>Panels</th>
+                  <th>
+                    Confidence <ToolTip :toolTipText="HELP_TEXT.CONFIDENCE" />
+                  </th>
+                  <th>Curator</th>
+                  <th>Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr
+                  v-for="item in geneExistingDrafts.results"
+                  :key="item.stable_id"
+                >
+                  <td>
+                    {{ item.stable_id }}
+                  </td>
+                  <td>
+                    {{ item.gene }}
+                  </td>
+                  <td>
+                    {{ item.disease_name }}
+                  </td>
+                  <td>{{ item.genotype }}</td>
+                  <td>
+                    <span v-if="item.panels?.length > 0">
+                      {{ item.panels.join(", ") }}
+                    </span>
+                  </td>
+                  <td>
+                    <span
+                      v-if="item.confidence"
+                      class="badge text-white"
+                      :style="{
+                        backgroundColor:
+                          CONFIDENCE_COLOR_MAP[item.confidence.toLowerCase()],
+                      }"
+                    >
+                      {{ item.confidence }}
+                    </span>
+                  </td>
+                  <td>
+                    Name:
+                    {{ item.curator_first + " " + item.curator_last_name }}
+                    <br />
+                    Email: {{ item.curator_email }}
+                  </td>
+                  <td class="text-nowrap">
+                    <router-link
+                      :to="`/lgd/update-draft/${item.stable_id}`"
+                      style="text-decoration: none"
+                      v-if="isUserCreatorOfDraft(item.curator_email)"
+                    >
+                      Update draft
+                    </router-link>
+                    <a
+                      :href="`mailto:${item.curator_email}`"
+                      style="text-decoration: none"
+                      target="_blank"
+                      v-else
+                    >
+                      Mail curator
+                    </a>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          <div class="row mx-1 justify-content-between">
+            <div class="col-2 px-0">
+              <button
+                type="button"
+                class="btn btn-primary"
+                style="float: left"
+                @click="fetchExistingGeneDraftsPreviousPage"
+                v-if="geneExistingDrafts.previous"
+              >
+                <i class="bi bi-arrow-left"></i> Previous Page
+              </button>
+            </div>
+            <div class="col-2 px-0">
+              <button
+                type="button"
+                class="btn btn-primary"
+                style="float: right"
+                @click="fetchExistingGeneDraftsNextPage"
+                v-if="geneExistingDrafts.next"
+              >
+                Next Page
+                <i class="bi bi-arrow-right"></i>
+              </button>
+            </div>
+          </div>
+        </div>
+        <div v-if="geneExistingRecords?.results?.length > 0">
+          <h3 class="pt-3">Published records for Gene</h3>
+          <p class="text-muted mb-0">
+            View existing records to add to your panel
+          </p>
+          <div class="mx-1 pt-3 table-responsive-xl">
+            <table class="table table-hover table-bordered">
+              <thead>
+                <tr>
+                  <th>G2P ID <ToolTip :toolTipText="HELP_TEXT.G2P_ID" /></th>
+                  <th>Gene</th>
+                  <th>Disease</th>
+                  <th>
+                    Allelic Requirement
+                    <ToolTip :toolTipText="HELP_TEXT.ALLELIC_REQUIREMENT" />
+                  </th>
+                  <th>
+                    Mechanism <ToolTip :toolTipText="HELP_TEXT.MECHANISM" />
+                  </th>
+                  <th>Panels</th>
+                  <th>
+                    Confidence <ToolTip :toolTipText="HELP_TEXT.CONFIDENCE" />
+                  </th>
+                  <th>Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr
+                  v-for="item in geneExistingRecords.results"
+                  :key="item.stable_id"
+                >
+                  <td>
+                    {{ item.stable_id }}
+                  </td>
+                  <td>
+                    <router-link
+                      v-if="item.gene"
+                      :to="`/gene/${item.gene}`"
+                      style="text-decoration: none"
+                    >
+                      {{ item.gene }}
+                    </router-link>
+                  </td>
+                  <td>
+                    <router-link
+                      v-if="item.disease"
+                      :to="`/disease/${item.disease}`"
+                      style="text-decoration: none"
+                    >
+                      {{ item.disease }}
+                    </router-link>
+                  </td>
+                  <td>{{ item.genotype }}</td>
+                  <td>{{ item.mechanism }}</td>
+                  <td>
+                    <span v-if="item.panel?.length > 0">
+                      <span v-for="(panelName, index) in item.panel">
+                        <span v-if="index < item.panel.length - 1">
+                          <router-link
+                            :to="`/panel/${panelName}`"
+                            style="text-decoration: none"
+                          >
+                            {{ panelName }} </router-link
+                          >,
+                        </span>
+                        <router-link
+                          :to="`/panel/${panelName}`"
+                          style="text-decoration: none"
+                          v-else
+                        >
+                          {{ panelName }}
+                        </router-link>
+                      </span>
+                    </span>
+                  </td>
+                  <td>
+                    <span
+                      v-if="item.confidence"
+                      class="badge text-white"
+                      :style="{
+                        backgroundColor:
+                          CONFIDENCE_COLOR_MAP[item.confidence.toLowerCase()],
+                      }"
+                    >
+                      {{ item.confidence }}
+                    </span>
+                  </td>
+                  <td class="text-nowrap">
+                    <router-link
+                      :to="`/lgd/${item.stable_id}`"
+                      style="text-decoration: none"
+                      target="_blank"
+                      v-if="item.stable_id"
+                    >
+                      View record <i class="bi bi-box-arrow-up-right"></i>
+                    </router-link>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          <div class="row mx-1 justify-content-between">
+            <div class="col-2 px-0">
+              <button
+                type="button"
+                class="btn btn-primary"
+                style="float: left"
+                @click="fetchExistingGeneRecordsPreviousPage"
+                v-if="geneExistingRecords.previous"
+              >
+                <i class="bi bi-arrow-left"></i> Previous Page
+              </button>
+            </div>
+            <div class="col-2 px-0">
+              <button
+                type="button"
+                class="btn btn-primary"
+                style="float: right"
+                @click="fetchExistingGeneRecordsNextPage"
+                v-if="geneExistingRecords.next"
+              >
+                Next Page
+                <i class="bi bi-arrow-right"></i>
+              </button>
+            </div>
+          </div>
+        </div>
+        <div class="my-3">
+          <h6 class="mb-3">
+            Adding a novel disease or an association with a different mechanism
+            or allelic requirement?
+          </h6>
+          <div>
+            <button
+              type="button"
+              class="btn btn-link m-0 p-0"
+              style="text-decoration: none"
+              data-bs-toggle="modal"
+              data-bs-target="#record-guidelines-modal"
+            >
+              Please read this information before creating a new record
+              <i class="bi bi-file-earmark-text"></i>
+            </button>
+          </div>
+          <button
+            type="button"
+            class="btn btn-primary mt-3"
+            @click="geneSearchBtnClickHandler"
+          >
+            Create new record
+          </button>
+        </div>
       </div>
     </div>
   </div>
