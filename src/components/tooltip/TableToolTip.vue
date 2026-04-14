eaef1d9b2d020ae785ad5f3d30d9b225e2b1db40
seeta-ramaraju <150829444+seeta-ramaraju@users.noreply.github.com>
2026-04-14 15:13:28 +0100

    In record page, change mined publications display to support search, sort

---
+++ src/components/tooltip/TableToolTip.vue
@@ -0,0 +1,32 @@
+<script>
+export default {
+  props: {
+    toolTipText: String,
+  },
+  mounted() {
+    if (!window.bootstrap?.Tooltip) return;
+    this.tooltip = new window.bootstrap.Tooltip(this.$refs.tooltipEl);
+  },
+  beforeUnmount() {
+    this.tooltip?.dispose();
+  },
+  data() {
+    return {
+      tooltip: null,
+    };
+  },
+};
+</script>
+<template>
+  <span
+    ref="tooltipEl"
+    class="bi bi-info-circle"
+    style="color: #0d6efd; font-size: 16px; cursor: pointer"
+    data-bs-toggle="tooltip"
+    data-bs-placement="top"
+    :data-bs-title="toolTipText"
+    tabindex="0"
+    role="button"
+    aria-label="More information"
+  ></span>
+</template>
